9062181ab0a6d0886154e6ca625583f2f942deea
piotrb <piotrbanko@me.com>
2023-12-27 18:14:17 +0100

    Added OpenaiAssistant class with basic functionality to operate with OpenAI API

--- openai_assistant.py
+++ openai_assistant.py
@@ -1,5 +1,6 @@
 import configparser
 import json
+import time
 
 from openai import OpenAI
 
@@ -24,6 +25,88 @@ def setup(client):
 
     thread = client.beta.threads.create()
     print(f"Created thread: {thread.id}")
+
+
+class OpenaiAssistant:
+    def __init__(self, client, assistant_id, thread_id):
+        self.client = client
+        self.assistant_id = assistant_id
+        self.thread_id = thread_id
+
+    def add_message_to_thread(self, message: str) -> None:
+        message = self.client.beta.threads.messages.create(
+            thread_id=self.thread_id,
+            role="user",
+            content=message
+        )
+        print(f"Added message to thread {self.thread_id}: {message}")
+
+    def send_command(self, instructions: str = ''):
+        run = self.client.beta.threads.runs.create(
+            thread_id=self.thread_id,
+            assistant_id=self.assistant_id,
+            instructions=instructions
+        )
+        print(f"Added command to thread: {self.thread_id} with instructions: {instructions}")
+
+        return run
+
+    def get_response(self, run_id):
+
+        # Check if the run is completed
+        run_status = None
+        while not run_status == "completed":
+            run = self.client.beta.threads.runs.retrieve(
+                thread_id=self.thread_id,
+                run_id=run_id
+            )
+            run_status = run.status
+
+            if run_status in ["failed", "expired", "stopped"]:
+                print(run)
+                raise Exception(f"Run {run_id} failed with status {run_status}")
+
+            if run_status == "requires_action":
+                print(f"Run {run_id} requires action")
+
+                print(run.required_action)
+
+                if run.required_action.type == "submit_tool_outputs":
+                    # action = run.required_action["submit_tool_outputs"]["tool_calls"][0]["function"]
+                    action = run.required_action.submit_tool_outputs.tool_calls[0].function
+                    call_id = run.required_action.submit_tool_outputs.tool_calls[0].id
+
+                    function_name = action.name
+                    function_args = action.arguments
+
+                    return {"message": None, "run_id": run_id, "call_id": call_id,
+                            "action": {"function_name": function_name, "function_args": function_args}}
+
+            time.sleep(1)
+
+        # Get the message back
+        messages = self.client.beta.threads.messages.list(
+            thread_id=self.thread_id
+        )
+
+        # Print the message content
+        print(f"Got response from thread {self.thread_id}: {messages.data[0].content[0].text.value}")
+
+        return {"message": messages.data[0].content[0].text.value, "run": None, "action": None}
+
+    def submit_tool_outputs(self, run_id: str, call_ids: str, output: str):
+        run = self.client.beta.threads.runs.submit_tool_outputs(
+            thread_id=self.thread_id,
+            run_id=run_id,
+            tool_outputs=[
+                {
+                    "tool_call_id": call_ids,
+                    "output": output,
+                }
+            ]
+        )
+
+        return run
 
 
 if __name__ == "__main__":
