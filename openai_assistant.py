fc9b4ed92e7c2997b6d5760493d86c75df48d4b4
piotrb <piotrbanko@me.com>
2023-12-27 13:40:28 +0100

    Initial commit with basic connection to the OpenAI API

---
+++ openai_assistant.py
@@ -0,0 +1,47 @@
+import configparser
+import json
+
+from openai import OpenAI
+
+
+def setup(client):
+    # Load the functions from the json file
+    with open('functions.json', 'r') as json_file:
+        functions = json.load(json_file)
+        print(f"Loaded functions: {functions}")
+
+    # Create a new assistant and thread
+    assistant = client.beta.assistants.create(
+        name="Telegram Assistant",
+        instructions="You are my personal assistant",
+        tools=functions,
+        model="gpt-4"
+    )
+
+    print(f"Created assistant: {assistant.id}")
+
+    thread = client.beta.threads.create()
+    print(f"Created thread: {thread.id}")
+
+
+if __name__ == "__main__":
+    # Get the api key from the JSON file
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    OPENAI_API_KEY = config['OPENAI']['OPENAI_API_KEY']
+
+    client_ = OpenAI(api_key=OPENAI_API_KEY)
+
+    setup(client_)
+
+    # completion = client.chat.completions.create(
+    #     model="gpt-4-1106-preview",
+    #     messages=[
+    #         {"role": "system",
+    #          "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    #         {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    #     ]
+    # )
+
+    # print(completion.choices[0].message)
