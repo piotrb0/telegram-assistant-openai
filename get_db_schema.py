1de7d725f3b7c0bc8ae4984402721b1b775e76dd
piotrb <piotrbanko@me.com>
2023-12-30 16:24:22 +0100

    Added the get_db_schema.py file so the bot can know how to get info from the database

---
+++ get_db_schema.py
@@ -0,0 +1,53 @@
+"""
+This module contains functions for getting the schema of the database.
+"""
+
+import sqlite3
+
+
+def get_table_names(conn):
+    """Return a list of table names."""
+    table_names = []
+    tables = conn.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    for table in tables.fetchall():
+        table_names.append(table[0])
+    return table_names
+
+
+def get_column_names(conn, table_name):
+    """Return a list of column names."""
+    column_names = []
+    columns = conn.execute(f"PRAGMA table_info('{table_name}');").fetchall()
+    for col in columns:
+        column_names.append(col[1])
+    return column_names
+
+
+def get_database_info(conn):
+    """Return a list of dicts containing the table name and columns for each table in the database."""
+    table_dicts = []
+    for table_name in get_table_names(conn):
+        columns_names = get_column_names(conn, table_name)
+        table_dicts.append({"table_name": table_name, "column_names": columns_names})
+    return table_dicts
+
+
+def get_schema():
+    with sqlite3.connect('assistant.db') as conn:
+        # cursor = conn.cursor()
+        # cursor.execute(query)
+        # return {'success': True, 'info': cursor.fetchall(), 'error': None}
+
+        database_schema_dict = get_database_info(conn)
+        database_schema_string = "\n".join(
+            [
+                f"Table: {table['table_name']}\nColumns: {', '.join(table['column_names'])}"
+                for table in database_schema_dict
+            ]
+        )
+
+    return database_schema_string
+
+
+if __name__ == '__main__':
+    print(get_schema())
