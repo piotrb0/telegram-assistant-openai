2d66ede4ae22eec2c2150898271b0bff10d177a3
piotrb <piotrbanko@me.com>
2024-01-02 19:03:34 +0100

    Added the cancel_openai_run.py file that allows you ro cancel unfinished OpenAI job if the program crashes

---
+++ cancel_openai_run.py
@@ -0,0 +1,22 @@
+"""
+Cancel a run in a OpenAI thread. Use it when you get an error while using the bot and want to run the assistant
+again with another run.
+"""
+from openai import OpenAI
+from data import OPENAI_API_KEY
+
+if __name__ == "__main__":
+    THREAD = ""
+
+    client = OpenAI(api_key=OPENAI_API_KEY)
+    runs = client.beta.threads.runs.list(THREAD)
+    print(runs)
+
+    for run in runs:
+        # Cancel the run if it's still running
+        if run.status == "running":
+            print(f"Canceling run {run.id}")
+            client.beta.threads.runs.cancel(
+                thread_id=THREAD,
+                run_id=run.id
+            )
